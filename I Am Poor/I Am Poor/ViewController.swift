f994fbd4efca0f1aad3440f7e1b45a559bb8b6ed
Ivan Romero <61719074+ivanmromero@users.noreply.github.com>
2022-01-23 20:57:53 -0300

    Add files via upload
    Final Proyect

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Ivan Romero on 02/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
