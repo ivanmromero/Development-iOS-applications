8e6342ed7d506ac965e487a2caf3c356e5704c55
Ivan Romero <61719074+ivanmromero@users.noreply.github.com>
2022-01-23 21:12:03 -0300

    Add files via upload
    Final Proyect

---
+++ IvanCard/IvanCard/InfoView.swift
@@ -0,0 +1,35 @@
+//
+//  InfoView.swift
+//  IvanCard
+//
+//  Created by Ivan Romero on 05/01/2022.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(
+                HStack{
+                    Image(systemName: imageName).foregroundColor(.green)
+                    Text(text)
+                    .foregroundColor(Color.black)
+                }
+            )
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
---
+++ IvanCard/IvanCard/IvanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IvanCardApp.swift
+//  IvanCard
+//
+//  Created by Ivan Romero on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct IvanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
