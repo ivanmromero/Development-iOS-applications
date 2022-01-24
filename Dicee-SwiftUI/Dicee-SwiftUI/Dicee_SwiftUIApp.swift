1ac2dd15e5bfd1932f2d51209ae5963d31efa9b5
Ivan Romero <61719074+ivanmromero@users.noreply.github.com>
2022-01-23 21:24:11 -0300

    Add files via upload
    Final Proyect

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Ivan Romero on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
